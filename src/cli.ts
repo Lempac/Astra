bf91838eea5160e51eb822eece5768dbc2653c1e
Lempac <brunis.biksis@gmail.com>
2025-04-22 01:05:32 +0300

    check for localappdata missing, update lock, option for target

--- src/cli.ts
+++ src/cli.ts
@@ -1,134 +1,187 @@
-import { CompilationTarget, compileAddon, compileDirectory, compileFile, CompilerConfig, getFileInfo, PackType, targetToPath } from "./lib.ts";
-import { parse } from "https://deno.land/std@0.181.0/flags/mod.ts";
-import { dirname, basename } from "https://deno.land/std@0.181.0/path/mod.ts";
-import { ensureDirSync } from "https://deno.land/std@0.181.0/fs/mod.ts";
+import {
+  CompilationTarget,
+  compileAddon,
+  compileDirectory,
+  compileFile,
+  CompilerConfig,
+  getFileInfo,
+  PackType,
+  targetToPath,
+} from "./lib.ts";
+import { parseArgs } from "jsr:@std/cli/parse-args";
+import { basename, dirname } from "jsr:@std/path";
+import { ensureDirSync } from "jsr:@std/fs";
+import { JscTarget } from "https://esm.sh/@swc/core@1.2.212/types.d.ts";
 
 const version = "v1.0.1";
-
 // Get CLI Flags
-const flags = parse(Deno.args, {
-    boolean: ["version", "v", "preview"],
-    alias: {
-        v: "version"
-    }
+const flags = parseArgs(Deno.args, {
+  boolean: ["version", "v", "preview"],
+  string: ["target"],
+  alias: {
+    v: "version",
+    t: "target",
+  },
 });
 
 // Exit if the flag for version was used
 if (flags.version) {
-    console.log(`Version ${version}`);
-    Deno.exit(0);
+  console.log(`Version ${version}`);
+  Deno.exit(0);
 }
 
 // If there were no flags passed
 if (flags._[0] === undefined) {
-    console.log("flags: [-v | --version] [--preview]");
-    console.log("commands: [watch] [package] [scaffold] [com.mojang]")
-    Deno.exit(1);
+  console.log(
+    "flags: [-v | --version] [--preview] [-t | --target]=[es3|es2020|esX]",
+  );
+  console.log("commands: [watch] [package] [scaffold] [com.mojang]");
+  Deno.exit(1);
 }
 
 const configPath = Deno.cwd() + "/compiler.config.json";
 const configExists = await getFileInfo(configPath) !== undefined;
 
 if (flags._[0] === "scaffold") {
-    // Don't create a new project if there is already a project in the CWD
-    if (configExists) {
-        console.log("compiler.config.json already found")
-        Deno.exit(1);
-    }
+  // Don't create a new project if there is already a project in the CWD
+  if (configExists) {
+    console.log("compiler.config.json already found");
+    Deno.exit(1);
+  }
 
-    const packName = prompt("Project Name:");
-    if (packName === null) {
-        console.log("Invalid Pack Name")
-        Deno.exit(1);
-    }
+  const packName = prompt("Project Name:");
+  if (packName === null) {
+    console.log("Invalid Pack Name");
+    Deno.exit(1);
+  }
 
-    const config: CompilerConfig = {
-        packName,
-        behaviourPackPath: "/BP/",
-        resourcePackPath: "/RP/"
-    }
+  const config: CompilerConfig = {
+    packName,
+    behaviourPackPath: "/BP/",
+    resourcePackPath: "/RP/",
+  };
 
-    Deno.writeTextFileSync(Deno.cwd() + "/compiler.config.json", JSON.stringify(config, null, 4));
-    ensureDirSync(Deno.cwd() + "/BP/");
-    ensureDirSync(Deno.cwd() + "/RP/");
+  Deno.writeTextFileSync(
+    Deno.cwd() + "/compiler.config.json",
+    JSON.stringify(config, null, 4),
+  );
+  ensureDirSync(Deno.cwd() + "/BP/");
+  ensureDirSync(Deno.cwd() + "/RP/");
 
-    Deno.writeTextFileSync(Deno.cwd() + "/.gitignore", "/dist/");
+  Deno.writeTextFileSync(Deno.cwd() + "/.gitignore", "/dist/");
 
-    Deno.run({ cmd: ["git", "init"], stdout: "null" });
+  new Deno.Command("git", { args: ["init"], stdout: "null" });
 
-    console.log("Initialized Project in directory");
-    console.log("Initialized Git Repository");
-    Deno.exit(0);
+  console.log("Initialized Project in directory");
+  console.log("Initialized Git Repository");
+  Deno.exit(0);
 }
 
 if (!configExists) {
-    console.log("compiler.config.json not found");
-    Deno.exit(1);
+  console.log("compiler.config.json not found");
+  Deno.exit(1);
 }
 
-const packConfig: CompilerConfig = JSON.parse(Deno.readTextFileSync(configPath))
+const packConfig: CompilerConfig = JSON.parse(
+  Deno.readTextFileSync(configPath),
+);
 
 if (flags._[0] === "watch") {
-    const target = flags.preview ? CompilationTarget.Preview : CompilationTarget.Stable;
-    await compileAddon(packConfig, target);
+  const target = flags.preview
+    ? CompilationTarget.Preview
+    : CompilationTarget.Stable;
+  await compileAddon(packConfig, target, flags.target as JscTarget);
 
-    console.log("Starting watch mode...")
-    // Todo: Watch both RP And BP
-    const watcher = Deno.watchFs(Deno.cwd());
+  console.log("Starting watch mode...");
+  // Todo: Watch both RP And BP
+  const watcher = Deno.watchFs(Deno.cwd());
 
-    const behaviourPath = Deno.cwd() + "/BP/";
-    const resourcePath = Deno.cwd() + "/RP/";
+  const behaviourPath = Deno.cwd() + "/BP/";
+  const resourcePath = Deno.cwd() + "/RP/";
 
-    for await (const event of watcher) {
-        // Ignore non addon stuff;
-        if (!event.paths[0].startsWith(behaviourPath) && !event.paths[0].startsWith(resourcePath)) continue;
-        
-        const packType = event.paths[0].startsWith(behaviourPath) ? PackType.Behaviour : PackType.Resource;
-        const relativePath = event.paths[0].replace(packType === PackType.Behaviour ? behaviourPath : resourcePath, "");
-        const fileInfo = await getFileInfo(event.paths[0]);
-        const destBasePath = targetToPath(target, packType, packConfig.packName);
-        const destPath = destBasePath + relativePath
+  for await (const event of watcher) {
+    // Ignore non addon stuff;
+    if (
+      !event.paths[0].startsWith(behaviourPath) &&
+      !event.paths[0].startsWith(resourcePath)
+    ) continue;
 
-        if (fileInfo === undefined) {
-            const destPathExists = await getFileInfo(destPath) !== undefined;
-            if (destPathExists) {
-                const start = performance.now();
-                await Deno.remove(destPath, { recursive: true });
-                console.log(`Removed: [${relativePath}] in ${performance.now() - start}ms`)
-            }
-            
-            continue;
-        }
+    const packType = event.paths[0].startsWith(behaviourPath)
+      ? PackType.Behaviour
+      : PackType.Resource;
+    const relativePath = event.paths[0].replace(
+      packType === PackType.Behaviour ? behaviourPath : resourcePath,
+      "",
+    );
+    const fileInfo = await getFileInfo(event.paths[0]);
+    const destBasePath = targetToPath(target, packType, packConfig.packName);
+    const destPath = destBasePath + relativePath;
 
-        if (fileInfo.isFile) {
-            const start = performance.now();
-            await compileFile(dirname(event.paths[0]), dirname(destPath), basename(event.paths[0]));
-            console.log(`Updated: [${relativePath}] in ${performance.now() - start}ms`)
-        }
+    if (fileInfo === undefined) {
+      const destPathExists = await getFileInfo(destPath) !== undefined;
+      if (destPathExists) {
+        const start = performance.now();
+        await Deno.remove(destPath, { recursive: true });
+        console.log(
+          `Removed: [${relativePath}] in ${performance.now() - start}ms`,
+        );
+      }
 
-        if (fileInfo.isDirectory) {
-            const start = performance.now();
-            const count = await compileDirectory(relativePath, target, packType, packConfig.packName);
-            const timeTaken = performance.now() - start;
-            console.log(`Compiled ${count} files in ${timeTaken}ms`);
-        }
+      continue;
     }
 
-    Deno.exit(0);
+    if (fileInfo.isFile) {
+      const start = performance.now();
+      await compileFile(
+        dirname(event.paths[0]),
+        dirname(destPath),
+        basename(event.paths[0]),
+        flags.target as JscTarget,
+      );
+      console.log(
+        `Updated: [${relativePath}] in ${performance.now() - start}ms`,
+      );
+    }
+
+    if (fileInfo.isDirectory) {
+      const start = performance.now();
+      const count = await compileDirectory(
+        relativePath,
+        target,
+        packType,
+        packConfig.packName,
+        flags.target as JscTarget,
+      );
+      const timeTaken = performance.now() - start;
+      console.log(`Compiled ${count} files in ${timeTaken}ms`);
+    }
+  }
+
+  Deno.exit(0);
 }
 
 if (flags._[0] === "package") {
-    await compileAddon(packConfig, CompilationTarget.Packaged);
-    Deno.exit(0);
+  await compileAddon(
+    packConfig,
+    CompilationTarget.Packaged,
+    flags.target as JscTarget,
+  );
+  Deno.exit(0);
 }
 
 if (flags._[0] === "outdir") {
-    const target = flags.preview ? CompilationTarget.Preview : CompilationTarget.Stable;
+  const target = flags.preview
+    ? CompilationTarget.Preview
+    : CompilationTarget.Stable;
 
-    if (packConfig.behaviourPackPath !== undefined) {
-        console.log(`BP: "${targetToPath(target, PackType.Behaviour, packConfig.packName)}"`)
-    }
-    if (packConfig.resourcePackPath !== undefined) {
-        console.log(`RP: "${targetToPath(target, PackType.Resource, packConfig.packName)}"`)
-    }
-} 
+  if (packConfig.behaviourPackPath !== undefined) {
+    console.log(
+      `BP: "${targetToPath(target, PackType.Behaviour, packConfig.packName)}"`,
+    );
+  }
+  if (packConfig.resourcePackPath !== undefined) {
+    console.log(
+      `RP: "${targetToPath(target, PackType.Resource, packConfig.packName)}"`,
+    );
+  }
+}
--- src/lib.ts
+++ src/lib.ts
@@ -1,127 +1,171 @@
-import { emptyDirSync, copy } from "https://deno.land/std@0.181.0/fs/mod.ts";
+import { copy, emptyDirSync } from "jsr:@std/fs";
 import { transform } from "https://deno.land/x/swc@0.2.1/mod.ts";
+import { JscTarget } from "https://esm.sh/@swc/core@1.2.212/types.d.ts";
 
-const localAppData = Deno.env.get('localappdata');
+const localAppData = Deno.env.get("localappdata");
+if (!localAppData) {
+  console.error("Env localappdata is missing.");
+  Deno.exit(-1);
+}
 const stablePackage = "Microsoft.MinecraftUWP_8wekyb3d8bbwe";
 const previewPackage = "Microsoft.MinecraftWindowsBeta_8wekyb3d8bbwe";
-const comMojang = "LocalState/games/com.mojang"
-
+const comMojang = "LocalState/games/com.mojang";
 
 /**
  * Stores the intended compilation target
  */
 export enum CompilationTarget {
-    /**Export directly as a .mcaddon File*/
-    Packaged,
-    /**Export to the stable version of minecraft */
-    Stable,
-    /**Export to the preview version of minecraft */
-    Preview
+  /**Export directly as a .mcaddon File*/
+  Packaged,
+  /**Export to the stable version of minecraft */
+  Stable,
+  /**Export to the preview version of minecraft */
+  Preview,
 }
 
 export enum PackType {
-    Behaviour,
-    Resource
+  Behaviour,
+  Resource,
 }
 
 /**
  * Stores settings related to the compiler
  */
 export interface CompilerConfig {
-    packName: string,
-    behaviourPackPath?: string,
-    resourcePackPath?: string
+  packName: string;
+  behaviourPackPath?: string;
+  resourcePackPath?: string;
 }
 
-export async function getFileInfo(path: string): Promise<Deno.FileInfo | undefined> {
+export async function getFileInfo(
+  path: string,
+): Promise<Deno.FileInfo | undefined> {
+  try {
+    const res = await Deno.stat(path);
+    return res;
+  } catch (err) {
+    if (err instanceof Deno.errors.NotFound) {
+      return undefined;
+    }
+
+    throw err;
+  }
+}
+
+export async function compileAddon(
+  config: CompilerConfig,
+  target: CompilationTarget,
+  ftarget: JscTarget,
+): Promise<void> {
+  let count = 0;
+  const start = performance.now();
+  console.log("Starting addon compilation...");
+
+  if (config.behaviourPackPath !== undefined) {
+    count += await compileDirectory(
+      "",
+      target,
+      PackType.Behaviour,
+      config.packName,
+      ftarget,
+    );
+  }
+
+  if (config.resourcePackPath !== undefined) {
+    count += await compileDirectory(
+      "",
+      target,
+      PackType.Resource,
+      config.packName,
+      ftarget,
+    );
+  }
+
+  const timeTaken = performance.now() - start;
+  console.log(`Compiled ${count} files in ${timeTaken}ms`);
+}
+
+export function targetToPath(
+  target: CompilationTarget,
+  packType: PackType,
+  packName: string,
+): string {
+  const packPath = packType === PackType.Behaviour ? "BP" : "RP";
+  const folderName = packType === PackType.Behaviour
+    ? "development_behavior_packs"
+    : "development_resource_packs";
+
+  if (target === CompilationTarget.Packaged) {
+    return `${Deno.cwd()}/dist/${packPath}/`;
+  } else if (target === CompilationTarget.Stable) {
+    return `${localAppData}/Packages/${stablePackage}/${comMojang}/${folderName}/${packName} ${packPath}/`;
+  } else if (target === CompilationTarget.Preview) {
+    return `${localAppData}/Packages/${previewPackage}/${comMojang}/${folderName}/${packName} ${packPath}/`;
+  }
+
+  throw new Error(`Unknown Pack Type ${packType}`);
+}
+
+export async function compileDirectory(
+  path: string,
+  target: CompilationTarget,
+  packType: PackType,
+  packName: string,
+  ftarget: JscTarget,
+): Promise<number> {
+  let fileCount = 0;
+  const packDir = Deno.cwd() +
+    (packType === PackType.Behaviour ? "/BP/" : "/RP/") + path;
+  const destDir = targetToPath(target, packType, packName) + path;
+  emptyDirSync(destDir);
+
+  // Go through each file & Directory
+  for await (const dirEntry of Deno.readDirSync(packDir)) {
+    if (dirEntry.isFile) {
+      fileCount += 1;
+      compileFile(packDir, destDir, dirEntry.name, ftarget);
+    } else if (dirEntry.isDirectory) {
+      fileCount += await compileDirectory(
+        path + `${dirEntry.name}/`,
+        target,
+        packType,
+        packName,
+        ftarget,
+      );
+    }
+  }
+
+  return fileCount;
+}
+
+export async function compileFile(
+  packPath: string,
+  destPath: string,
+  fileName: string,
+  target: JscTarget = "es2021",
+) {
+  if (fileName.endsWith(".ts")) {
+    const fileText = await Deno.readTextFile(packPath + `/${fileName}`);
     try {
-        const res = await Deno.stat(path);
-        return res;
+      const transpiledJs = transform(fileText, {
+        jsc: {
+          target: target,
+          parser: {
+            syntax: "typescript",
+          },
+        },
+      }).code;
+
+      await Deno.writeTextFile(
+        destPath + `/${fileName.replace(".ts", ".js")}`,
+        transpiledJs,
+      );
+    } catch (_e) {
+      console.log(`Failed to compile ${fileName}`);
     }
-    catch (err) {
-        if (err instanceof Deno.errors.NotFound) {
-            return undefined;
-        }
-
-        throw err;
-    }
-}
-
-export async function compileAddon(config: CompilerConfig, target: CompilationTarget): Promise<void> {
-    let count = 0;
-    const start = performance.now();
-    console.log("Starting addon compilation...")
-
-    if (config.behaviourPackPath !== undefined) {
-        count += await compileDirectory("", target, PackType.Behaviour, config.packName);
-    }
-
-    if (config.resourcePackPath !== undefined) {
-        count += await compileDirectory("", target, PackType.Resource, config.packName);
-    }
-
-    const timeTaken = performance.now() - start;
-    console.log(`Compiled ${count} files in ${timeTaken}ms`)
-}
-
-export function targetToPath(target: CompilationTarget, packType: PackType, packName: string): string {
-    const packPath = packType === PackType.Behaviour ? "BP" : "RP";
-    const folderName = packType === PackType.Behaviour ? "development_behavior_packs" : "development_resource_packs";
-
-    if (target === CompilationTarget.Packaged) {
-        return `${Deno.cwd()}/dist/${packPath}/`
-    }
-
-    else if (target === CompilationTarget.Stable) {
-        return `${localAppData}/Packages/${stablePackage}/${comMojang}/${folderName}/${packName} ${packPath}/`
-    }
-
-    else if (target === CompilationTarget.Preview) {
-        return `${localAppData}/Packages/${previewPackage}/${comMojang}/${folderName}/${packName} ${packPath}/`
-    }
-
-    throw new Error(`Unknown Pack Type ${packType}`);
-}
-
-export async function compileDirectory(path: string, target: CompilationTarget, packType: PackType, packName: string): Promise<number> {
-    let fileCount = 0;
-    const packDir = Deno.cwd() + (packType === PackType.Behaviour ? "/BP/" : "/RP/") + path;
-    const destDir = targetToPath(target, packType, packName) + path;
-    emptyDirSync(destDir);
-
-    // Go through each file & Directory
-    for await (const dirEntry of Deno.readDirSync(packDir)) {
-        if (dirEntry.isFile) {
-            fileCount += 1;
-            compileFile(packDir, destDir, dirEntry.name);
-        }
-        else if (dirEntry.isDirectory) {
-            fileCount += await compileDirectory(path + `${dirEntry.name}/`, target, packType, packName)
-        }
-    }
-
-    return fileCount;
-}
-
-export async function compileFile(packPath: string, destPath: string, fileName: string) {
-    if (fileName.endsWith(".ts")) {
-        const fileText = await Deno.readTextFile(packPath + `/${fileName}`);
-        try {
-            const transpiledJs = transform(fileText, {
-                jsc: {
-                    target: "es2021",
-                    parser: {
-                        syntax: "typescript",
-                    },
-                },
-            }).code;
-
-            await Deno.writeTextFile(destPath + `/${fileName.replace(".ts", ".js")}`, transpiledJs)
-        } catch (e) {
-            console.log(`Failed to compile ${fileName}`)
-        } 
-    }
-    else {
-        await copy(`${packPath}/${fileName}`, `${destPath}/${fileName}`, { overwrite: true })
-    }
+  } else {
+    await copy(`${packPath}/${fileName}`, `${destPath}/${fileName}`, {
+      overwrite: true,
+    });
+  }
 }
